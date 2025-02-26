39022ad1e906793f90824d63a9bb3d62adffba7c
Mottel Zirkind <shachna@gmail.com>
2025-02-26 12:54:33 -0500

    quick style fix

--- lecture03/index.js
+++ lecture03/index.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
   res.send("It works!!!");
-})
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on http://localhost:${port}`);
